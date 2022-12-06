9349b4b24578bcac3889101497a6360b387f6621
Donovan Jimenez <donovan.jimenez@gmail.com>
2022-12-05 21:11:14 -0500

    2022 day 05 - slightly prettier, a lot less allocations (i think)

--- aoc2022/src/day05/mod.rs
+++ aoc2022/src/day05/mod.rs
@@ -8,18 +8,11 @@ pub fn run() {
 fn part1(input: &str) -> String {
     let (mut stacks, instructions) = parse_input(input);
 
-    for instruction in instructions {
-        let parts: Vec<&str> = instruction.split_whitespace().collect();
-
-        let count = parts[1].parse::<usize>().unwrap();
-        let from = parts[3].parse::<usize>().unwrap() - 1;
-        let to = parts[5].parse::<usize>().unwrap() - 1;
-
+    // assumes count > 0 and from != to
+    for (count, from, to) in instructions {
         for _ in 0..count {
-            if stacks[from].len() > 0 {
-                let value = stacks[from].pop().unwrap();
-                stacks[to].push(value);
-            }
+            let value = stacks[from].pop().unwrap();
+            stacks[to].push(value);
         }
     }
 
@@ -32,17 +25,17 @@ fn part1(input: &str) -> String {
 
 fn part2(input: &str) -> String {
     let (mut stacks, instructions) = parse_input(input);
+    let mut values: Vec<char> = Vec::new();
 
-    for instruction in instructions {
-        let parts: Vec<&str> = instruction.split_whitespace().collect();
-
-        let count = parts[1].parse::<usize>().unwrap();
-        let from = parts[3].parse::<usize>().unwrap() - 1;
-        let to = parts[5].parse::<usize>().unwrap() - 1;
-
+    // assumes count > 0 and from != to
+    for (count, from, to) in instructions {
         let from_idx = stacks[from].len() - count;
-        let values: Vec<char> = stacks[from].drain(from_idx..).collect();
-        stacks[to].extend_from_slice(&values[..]);
+
+        // we can't mut borrow multiple indexes of the same vec, so collect values into reused char vec
+        values.extend(stacks[from].drain(from_idx..));
+        stacks[to].extend_from_slice(values.as_slice());
+
+        values.clear();
     }
 
     // collect top letters
@@ -52,10 +45,7 @@ fn part2(input: &str) -> String {
         .collect::<String>()
 }
 
-fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<&str>) {
-    // will hold crate letters
-    let mut stacks = Vec::new();
-
+fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<(usize, usize, usize)>) {
     let mut sections = input.split("\r\n\r\n");
 
     let crates = sections.next().unwrap();
@@ -64,14 +54,17 @@ fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<&str>) {
     // simpler to parse crates in reverse line order
     let mut crate_lines = crates.lines().rev().map(|line| line.trim_end());
 
-    // the first line will tell us how many stacks there are
+    // the first (reversed) line will tell us how many stacks there are
     let stack_ids: Vec<&str> = crate_lines.next().unwrap().split_whitespace().collect();
+    let mut stacks = Vec::with_capacity(stack_ids.len());
 
     for _ in stack_ids {
         stacks.push(Vec::new());
     }
 
+    // NOTE: we consumed the label line already
     for crate_line in crate_lines {
+        // look for alpha characters, and convert their position in the line to a stack index
         for (crate_line_idx, crate_line_char) in crate_line.chars().enumerate() {
             if crate_line_char.is_alphabetic() {
                 let stack_idx = (crate_line_idx - 1) / 4;
@@ -80,10 +73,25 @@ fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<&str>) {
         }
     }
 
-    (
-        stacks,
-        instructions.lines().map(|line| line.trim_end()).collect(),
-    )
+    let mut parts: Vec<&str> = Vec::with_capacity(5);
+
+    // parse instruction lines into tuples (count, from, to)
+    let parsed_instructions = instructions
+        .lines()
+        .map(|instruction| {
+            parts.extend(instruction.trim_end().split_whitespace());
+
+            let count = parts[1].parse::<usize>().unwrap();
+            let from = parts[3].parse::<usize>().unwrap() - 1;
+            let to = parts[5].parse::<usize>().unwrap() - 1;
+
+            parts.clear();
+
+            (count, from, to)
+        })
+        .collect();
+
+    (stacks, parsed_instructions)
 }
 
 #[cfg(test)]
