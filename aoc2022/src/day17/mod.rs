3b4f858ca760f4cb9b587783bb0a26097cc72cdf
Donovan Jimenez <donovan.jimenez@gmail.com>
2022-12-21 20:16:53 -0500

    2022 day 17 part 2 rework
     * use Vec<u8> for rows
     * finally get the pattern matching to work

--- aoc2022/src/day17/mod.rs
+++ aoc2022/src/day17/mod.rs
@@ -112,7 +112,7 @@ impl Piece {
 struct Game {
     pieces: usize,
     offset: usize,
-    columns: [Vec<bool>; 7],
+    rows: Vec<u8>,
 }
 
 impl Game {
@@ -120,15 +120,15 @@ impl Game {
         Self {
             pieces: 0,
             offset: 0,
-            columns: [vec![], vec![], vec![], vec![], vec![], vec![], vec![]],
+            rows: Vec::new(),
         }
     }
 
     fn max_height(&self) -> usize {
-        self.columns
+        self.rows
             .iter()
-            .filter_map(|column| column.iter().rposition(|c| *c).map(|idx| idx + 1))
-            .max()
+            .rposition(|row| *row > 0)
+            .map(|i| i + 1)
             .unwrap_or(0)
     }
 
@@ -137,126 +137,107 @@ impl Game {
 
         match piece.shape {
             Shape::HorizonalBar => {
-                for x in piece.x..piece.x + 4 {
-                    while self.columns[x].len() < piece.y + 1 {
-                        self.columns[x].push(false);
-                    }
+                while self.rows.len() < piece.y + 1 {
+                    self.rows.push(0);
                 }
 
                 let &Piece { x, y, .. } = piece;
-
-                self.columns[x + 0][y + 0] = true;
-                self.columns[x + 1][y + 0] = true;
-                self.columns[x + 2][y + 0] = true;
-                self.columns[x + 3][y + 0] = true;
+                self.rows[y + 0] |= 0b01111000 >> x;
             }
             Shape::Plus => {
-                for x in piece.x..piece.x + 3 {
-                    while self.columns[x].len() < piece.y + 3 {
-                        self.columns[x].push(false);
-                    }
+                while self.rows.len() < piece.y + 3 {
+                    self.rows.push(0);
                 }
 
                 let &Piece { x, y, .. } = piece;
 
-                self.columns[x + 0][y + 1] = true;
-                self.columns[x + 1][y + 0] = true;
-                self.columns[x + 1][y + 1] = true;
-                self.columns[x + 1][y + 2] = true;
-                self.columns[x + 2][y + 1] = true;
+                self.rows[y + 2] |= 0b00100000 >> x;
+                self.rows[y + 1] |= 0b01110000 >> x;
+                self.rows[y + 0] |= 0b00100000 >> x;
             }
             Shape::BackwardsL => {
-                for x in piece.x..piece.x + 3 {
-                    while self.columns[x].len() < piece.y + 3 {
-                        self.columns[x].push(false);
-                    }
+                while self.rows.len() < piece.y + 3 {
+                    self.rows.push(0);
                 }
 
                 let &Piece { x, y, .. } = piece;
 
-                self.columns[x + 0][y + 0] = true;
-                self.columns[x + 1][y + 0] = true;
-                self.columns[x + 2][y + 0] = true;
-                self.columns[x + 2][y + 1] = true;
-                self.columns[x + 2][y + 2] = true;
+                self.rows[y + 2] |= 0b00010000 >> x;
+                self.rows[y + 1] |= 0b00010000 >> x;
+                self.rows[y + 0] |= 0b01110000 >> x;
             }
             Shape::VerticalBar => {
-                while self.columns[piece.x].len() < piece.y + 4 {
-                    self.columns[piece.x].push(false);
+                while self.rows.len() < piece.y + 4 {
+                    self.rows.push(0);
                 }
 
                 let &Piece { x, y, .. } = piece;
 
-                self.columns[x + 0][y + 0] = true;
-                self.columns[x + 0][y + 1] = true;
-                self.columns[x + 0][y + 2] = true;
-                self.columns[x + 0][y + 3] = true;
+                self.rows[y + 3] |= 0b01000000 >> x;
+                self.rows[y + 2] |= 0b01000000 >> x;
+                self.rows[y + 1] |= 0b01000000 >> x;
+                self.rows[y + 0] |= 0b01000000 >> x;
             }
             Shape::Square => {
-                for x in piece.x..piece.x + 2 {
-                    while self.columns[x].len() < piece.y + 2 {
-                        self.columns[x].push(false);
-                    }
+                while self.rows.len() < piece.y + 2 {
+                    self.rows.push(0);
                 }
 
                 let &Piece { x, y, .. } = piece;
 
-                self.columns[x + 0][y + 0] = true;
-                self.columns[x + 0][y + 1] = true;
-                self.columns[x + 1][y + 0] = true;
-                self.columns[x + 1][y + 1] = true;
+                self.rows[y + 1] |= 0b01100000 >> x;
+                self.rows[y + 0] |= 0b01100000 >> x;
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    fn collapse(&mut self) -> bool {
+        // flood fill spaces from the top, see what we can reach
+        let mut reachable = HashMap::new();
+        let mut queue = VecDeque::new();
+
+        let max_height = self.max_height();
+        let mut y_min = max_height;
+
+        queue.push_back((0, max_height));
+
+        while queue.len() > 0 {
+            let (x, y) = queue.pop_front().unwrap();
+
+            if !reachable.contains_key(&(x, y)) {
+                let empty = self.is_empty(x, y);
+                reachable.insert((x, y), empty);
+
+                if empty {
+                    if y < y_min {
+                        y_min = y;
+                    }
+
+                    if x > 0 {
+                        queue.push_back((x - 1, y));
+                    }
+                    if x < 7 {
+                        queue.push_back((x + 1, y));
+                    }
+                    if y > 0 {
+                        queue.push_back((x, y - 1));
+                    }
+                    if y < max_height {
+                        queue.push_back((x, y + 1));
+                    }
+                }
             }
         }
 
-        // flood fill spaces from the top, see what we can reach
-        let max_height = self.max_height() + 1;
+        // truncate upto y_min
+        if y_min < max_height && y_min > 0 {
+            self.rows.drain(0..y_min - 1);
+            self.offset += y_min - 1;
 
-        if max_height > 1000000 {
-            let mut reachable = HashMap::new();
-            let mut queue = VecDeque::new();
-
-            let mut y_min = max_height;
-
-            queue.push_back((0, max_height));
-
-            while queue.len() > 0 {
-                let (x, y) = queue.pop_front().unwrap();
-
-                if !reachable.contains_key(&(x, y)) {
-                    let empty = self.is_empty(x, y);
-                    reachable.insert((x, y), empty);
-
-                    if empty {
-                        if y < y_min {
-                            y_min = y;
-                        }
-
-                        if x > 0 {
-                            queue.push_back((x - 1, y));
-                        }
-                        if x < 7 {
-                            queue.push_back((x + 1, y));
-                        }
-                        if y > 0 {
-                            queue.push_back((x, y - 1));
-                        }
-                        if y < max_height {
-                            queue.push_back((x, y + 1));
-                        }
-                    }
-                }
-            }
-
-            // truncate upto y_min
-            if y_min < max_height && y_min > 0 {
-                //println!("truncated {} lines", y_min);
-
-                for x in 0..7 {
-                    self.columns[x].drain(0..y_min);
-                }
-
-                self.offset += y_min;
-            }
+            true
+        } else {
+            false
         }
     }
 
@@ -277,7 +258,30 @@ impl Game {
     }
 
     fn is_empty(&self, x: usize, y: usize) -> bool {
-        x <= 6 && !*self.columns[x].get(y).unwrap_or(&false)
+        if y < self.rows.len() {
+            (x <= 6) && (self.rows[y] & (0b01000000 >> x) == 0)
+        } else {
+            x <= 6
+        }
+    }
+
+    #[allow(dead_code)]
+    fn print(&self) {
+        let max_height = self.max_height();
+
+        println!("{}", repeat('-').take(max_height).collect::<String>());
+        for x in 0..7 {
+            let mask = 0b01000000 >> x;
+
+            println!(
+                "{}",
+                self.rows
+                    .iter()
+                    .map(|row| if row & mask == 0 { '.' } else { '#' })
+                    .collect::<String>()
+            );
+        }
+        println!("{}", repeat('-').take(max_height).collect::<String>());
     }
 }
 
@@ -296,23 +300,7 @@ fn part1(input: &str) -> usize {
             y: game.max_height() + 3,
         };
 
-        /*
-        println!("=== NEW PIECE ===");
-        println!("{:?}", piece);
-        println!("=== NEW PIECE ===");
-        */
-
-        /*
-        println!("{}", repeat('-').take(game.max_height()).collect::<String>());
-        for column in game.columns.iter() {
-            println!("{}", column.iter().map(|c| if *c { '#' } else { '.' }).collect::<String>())
-        }
-        println!("{}", repeat('-').take(game.max_height()).collect::<String>());
-        */
-
         while game.step(&mut piece, moves.next().unwrap()) {}
-
-        //println!("Came to rest: {:?}", &piece);
 
         // solidify the piece in the game board
         game.add(&piece);
@@ -323,12 +311,100 @@ fn part1(input: &str) -> usize {
 
 fn part2(input: &str) -> usize {
     let parsed_moves = parse_input(input);
-    let mut shapes = SHAPES.iter().cycle();
-    let mut moves = parsed_moves.iter().cycle();
 
+    // For test, if we visualize how much height and how many movements are used on each round of shapes,
+    // we'll start to see a pattern from the 4th round on with period 7 rounds, similar to:
+    //
+    // height: +11 movements: +26
+    // height: +7 movements: +27
+    // height: +8 movements: +30
+    // height: +9 movements: +29
+    // height: +6 movements: +27
+    // height: +6 movements: +29
+    // height: +6 movements: +32
+    //
+    // We want to look for a cycle automatically in test and input data so we can skip simulating large chunks
+
+    let mut moves = parsed_moves.iter().cycle();
     let mut game = Game::new();
 
-    while game.pieces < 1000000000000 {
+    // switch the logic to play the initial game in full cycles of shapes
+    while game.pieces < 2022 {
+        for shape in &SHAPES {
+            // piece is 2 from left and 3 from top of all fixed pieces
+            let mut piece = Piece {
+                shape: shape.clone(),
+                x: 2,
+                y: game.max_height() + 3,
+            };
+
+            while game.step(&mut piece, moves.next().unwrap()) {}
+
+            // solidify the piece in the game board
+            game.add(&piece);
+        }
+    }
+
+    let search_start = game.max_height();
+
+    // do a single round
+    for shape in &SHAPES {
+        // piece is 2 from left and 3 from top of all fixed pieces
+        let mut piece = Piece {
+            shape: shape.clone(),
+            x: 2,
+            y: game.max_height() + 3,
+        };
+
+        while game.step(&mut piece, moves.next().unwrap()) {}
+
+        // solidify the piece in the game board
+        game.add(&piece);
+    }
+
+    let search_end = game.max_height();
+    let search_pieces = game.pieces;
+
+    // now continue rounds until we find a match
+    loop {
+        let candidate_start = game.max_height();
+
+        for shape in &SHAPES {
+            // piece is 2 from left and 3 from top of all fixed pieces
+            let mut piece = Piece {
+                shape: shape.clone(),
+                x: 2,
+                y: game.max_height() + 3,
+            };
+
+            while game.step(&mut piece, moves.next().unwrap()) {}
+
+            // solidify the piece in the game board
+            game.add(&piece);
+        }
+
+        let candidate_end = game.max_height();
+        let candidate_pieces = game.pieces;
+
+        if game.rows[search_start..search_end] == game.rows[candidate_start..candidate_end] {
+            let aligned_pieces = candidate_pieces - search_pieces;
+            let aligned_height = candidate_end - search_end;
+
+            let pieces_needed = 1_000_000_000_000 - game.pieces;
+            let alignments = pieces_needed / aligned_pieces;
+
+            // skiiiiiiiiiiiiiiiiiiiiiip...
+            game.offset += alignments * aligned_height;
+            game.pieces += alignments * aligned_pieces;
+
+            break;
+        }
+    }
+
+    // continue the simulation for any more pieces needed
+    let mut shapes = SHAPES.iter().cycle();
+
+    while game.pieces < 1_000_000_000_000 {
         // piece is 2 from left and 3 from top of all fixed pieces
         let mut piece = Piece {
             shape: shapes.next().unwrap().clone(),
@@ -336,23 +412,7 @@ fn part2(input: &str) -> usize {
             y: game.max_height() + 3,
         };
 
-        /*
-        println!("=== NEW PIECE ===");
-        println!("{:?}", piece);
-        println!("=== NEW PIECE ===");
-        */
-
-        /*
-        println!("{}", repeat('-').take(game.max_height()).collect::<String>());
-        for column in game.columns.iter() {
-            println!("{}", column.iter().map(|c| if *c { '#' } else { '.' }).collect::<String>())
-        }
-        println!("{}", repeat('-').take(game.max_height()).collect::<String>());
-        */
-
         while game.step(&mut piece, moves.next().unwrap()) {}
-
-        //println!("Came to rest: {:?}", &piece);
 
         // solidify the piece in the game board
         game.add(&piece);
